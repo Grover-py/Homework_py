655998a07872c5a5dccef0b4faaccf4cde3e48ba
Grover_py <Nebu23z@yandex.ru>
2021-02-09 21:47:39 +0300

    finish 7 homework

--- task_1.py
+++ task_1.py
@@ -1,20 +1,24 @@
-import time
-class TrafficLight:
-    __color = 'red'
 
-    def running(self):
-        n = 0
-        while n != 3:
-            TrafficLight.__color = 'red'
-            print(TrafficLight.__color)
-            time.sleep(7)
-            TrafficLight.__color = 'yellow'
-            print(TrafficLight.__color)
-            time.sleep(2)
-            TrafficLight.__color = 'green'
-            print(TrafficLight.__color)
-            time.sleep(4)
-            n += 1
+class Matrix():
+    def __init__(self, my_list):
+        self.my_list = my_list
 
-t = TrafficLight
-t.running('')
+    def __str__(self):
+        for el in self.my_list:
+            s = ''
+            for i in el:
+                s = f'{str(s)}   {str(i)}'
+            print(s)
+        return ''
+
+    def __add__(self, other):
+        for el in range(len(self.my_list)):
+            for i in range(len(other.my_list[el])):
+                self.my_list[el][i] = self.my_list[el][i] + other.my_list[el][i]
+        return Matrix.__str__(self)
+
+
+matrix = Matrix([[1, 2, 3, 7], [2, 5, 7, 1], [7, 6, 8, 4]])
+print(matrix)
+matrix_2 = Matrix([[4, 1, 5, 2], [1, 2, 1, 7], [15, 4, -8, 0]])
+print(matrix.__add__(matrix_2))
